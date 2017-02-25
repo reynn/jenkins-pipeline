07e349c98c7cd10f675aba0f133e7eeb123e6f6c
Nic Patterson <arasureynn@gmail.com>
2017-02-25 02:45:11 -0800

    Rename to nodeDocker to avoid confusion
    update slackmessage to maybe work?

--- vars/sendSlackMessage.groovy
+++ vars/sendSlackMessage.groovy
@@ -1,13 +1,20 @@
-def call(body) {
-  // evaluate the body block, and collect configuration into the object
-  def config = [:]
-  body.resolveStrategy = Closure.DELEGATE_FIRST
-  body.delegate = config
-  body()
+def call(msg, color, channel, domain, credentialsId) {
 
   def msg = config.msg ?: '',
-  def msgColor = config.msgColor ?: 'good',
+  def color = config.color ?: 'good',
+  def channel = config.channel ?: '',
+  def domain = config.domain ?: '',
   def credentialsId = config.credentialsId ?: 'slack-integration-token'
 
-  slackSend message: msg, color: msgColor, tokenCredentialId: credentialsId
+  try {
+    slackSend message: msg, color: color, channel: channel, domain: domain, tokenCredentialId: credentialsId
+  } catch (java.lang.NoSuchMethodError nsme) {
+    println "Slack plugin does not appear to be installed on this Jenkins server."
+  } catch (Exception e) {
+    println "Unable to send notification to Slack."
+    println "Exception details -----------------------"
+    println e
+    println "End exception details -------------------"
+  }
+
 }
